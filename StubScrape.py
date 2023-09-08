3e88f69648202243ab82b5a403d4e46c4e52bec0
Lang, Lambert <lambert.lang@student.tugraz.at>
2023-09-08 21:33:24 +0200

    Added Zeitungs-Scraping

--- StubScrape.py
+++ StubScrape.py
@@ -1,11 +1,14 @@
 import requests
 import itertools
 import string
-
 from bs4 import BeautifulSoup
 
+ZEITUNG_BASE_URL = "https://anno.onb.ac.at/cgi-content/anno?aid={}"
+ZEITSCHRIFT_BASE_URL = "https://anno.onb.ac.at/cgi-content/anno-plus?aid={}&datum=1900"
 
-def main():
+
+def scrapeStubs(base_url):
+
     # Generate the next all valid three letter combinations in alphabetical order starting from the one specified here
     start_combination = ('a', 'a', 'a')
     combinations = [''.join(comb) for comb in itertools.islice(itertools.product(string.ascii_lowercase, repeat=3),
@@ -13,7 +16,6 @@ def main():
                                                                                       repeat=3)).index(
                                                                    start_combination), None)]
 
-    base_url = "https://anno.onb.ac.at/cgi-content/anno-plus?aid={}&datum=1900"
     filename = "validstubs.json"
 
     x = 0
@@ -25,13 +27,22 @@ def main():
         try:
             # Extract the content of the title tag
             title_content = soup.title.string.strip()
+            #print(title_content)
 
             # Check if the title content contains a valid title
             # (ANNO doesn't return an error if the page is blank. so have to analyze the contents)
-            if title_content != "ÖNB-ANNO -" and not soup.find("h3", class_="not_exists"):
-                # Write the valid stub to file as full link
-                with open(filename, 'a') as file:
-                    file.write("https://anno.onb.ac.at/cgi-content/anno-plus?aid=" + comb + "&datum=1900" + '\n')
+            # Also, the formatting is different between "Zeitung" and "Zeitschrift" yet again.
+            if base_url == ZEITSCHRIFT_BASE_URL:
+                if title_content != "ÖNB-ANNO -" and not soup.find("h3", class_="not_exists"):
+                    # Write the valid stub to file as full link
+                    with open(filename, 'a') as file:
+                        file.write("https://anno.onb.ac.at/cgi-content/anno-plus?aid=" + comb + "&datum=1900" + '\n')
+                    print("Found one!")
+
+            elif base_url == ZEITUNG_BASE_URL:
+                if title_content != "ÖNB/ANNO AustriaN Newspaper Online" and not soup.find("h3", class_="not_exists"):
+                    with open(filename, 'a') as file:
+                        file.write("https://anno.onb.ac.at/cgi-content/anno?aid=" + comb + '\n')
                     print("Found one!")
 
         except AttributeError:
@@ -39,6 +50,12 @@ def main():
 
         print("Checked " + comb + " as combination No. " + str(x))
         x += 1
+
+
+def main():
+    # ANNO has different structures for "Zeitschriften" and "Zeitungen". So we can swap what hardcoded base url
+    # we want to pass deepending on what type of publication we want to get the stubs for here
+    scrapeStubs(ZEITUNG_BASE_URL)
 
 
 if __name__ == "__main__":
