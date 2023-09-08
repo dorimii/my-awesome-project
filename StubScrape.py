e0e0164f7ec58f9f50c7f514e6fed8def78b4664
Lang, Lambert <lambert.lang@student.tugraz.at>
2023-09-08 09:56:09 +0200

    Getting all valid ANNO stubs

--- StubScrape.py
+++ StubScrape.py
@@ -6,35 +6,39 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    # Generate the next ten three-letter combinations starting from "bdh"
-    combinations = [''.join(comb) for comb in itertools.product(string.ascii_lowercase, repeat=3)]
+    # Generate the next all valid three letter combinations in alphabetical order starting from the one specified here
+    start_combination = ('a', 'a', 'a')
+    combinations = [''.join(comb) for comb in itertools.islice(itertools.product(string.ascii_lowercase, repeat=3),
+                                                               list(itertools.product(string.ascii_lowercase,
+                                                                                      repeat=3)).index(
+                                                                   start_combination), None)]
 
     base_url = "https://anno.onb.ac.at/cgi-content/anno-plus?aid={}&datum=1900"
     filename = "validstubs.json"
 
     x = 0
-
     for comb in combinations:
         response = requests.get(base_url.format(comb))
 
-        # Parse the content with BeautifulSoup
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Extract the content of the title tag
-        title_content = soup.title.string.strip()
+        try:
+            # Extract the content of the title tag
+            title_content = soup.title.string.strip()
 
-        # Check if the title content is more than just "ÖNB-ANNO - "
-        if title_content != "ÖNB-ANNO -":
-            # Write the valid combination to a file
-            with open(filename, 'a') as file:
-                file.write("https://anno.onb.ac.at/cgi-content/anno-plus?aid=" + comb + "&datum=1900" + '\n')
-                print("Found one!")
+            # Check if the title content contains a valid title
+            # (ANNO doesn't return an error if the page is blank. so have to analyze the contents)
+            if title_content != "ÖNB-ANNO -" and not soup.find("h3", class_="not_exists"):
+                # Write the valid stub to file as full link
+                with open(filename, 'a') as file:
+                    file.write("https://anno.onb.ac.at/cgi-content/anno-plus?aid=" + comb + "&datum=1900" + '\n')
+                    print("Found one!")
 
-        print("Checked " + comb + " as combination " + str(x))
+        except AttributeError:
+            continue
+
+        print("Checked " + comb + " as combination No. " + str(x))
         x += 1
-
-    # This will print out the ten combinations checked for reference
-    # print("Checked combinations:", combinations)
 
 
 if __name__ == "__main__":
