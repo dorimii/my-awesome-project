2ac596db9e3e111a47c450a9b8e682a3674c3d04
Lang, Lambert <lambert.lang@student.tugraz.at>
2023-09-10 13:46:20 +0200

    Cleaner Soup Procurement

--- CheckContent.py
+++ CheckContent.py
@@ -72,25 +72,27 @@ def getPublicationTitle(soup, mode):
     return publication_title
 
 
-def getLinks(mode, soup):
+def getDiffPublicationModeLinks(mode, soup, content_zeitschrift, content_zeitung):
     # Again different based on the two datasets
     if mode == ZEITSCHRIFT_BASE_URL:
-        magazine_links = extractLinks(soup, ['anno-plus?', '&datum'])
+        magazine_links = extractLinks(soup, content_zeitschrift)
 
     else:
-        magazine_links = extractLinks(soup, ['anno?', '&datum'])
+        magazine_links = extractLinks(soup, content_zeitung)
 
     return magazine_links
 
 
 def scrapeTextAndSave(mode, url):
+
     soup = fetchPage(url)
 
     publication_title = getPublicationTitle(soup, mode)
 
-    magazine_links = getLinks(mode, soup)
+    magazine_links = getDiffPublicationModeLinks(mode, soup, ['anno-plus?', '&datum'], ['anno?', '&datum'])
 
     filtered_links = filterMagazinesByYearRange(magazine_links)
+
     if len(filtered_links) == 0:  # No publications found
         print("Dis empty!")
         return
@@ -99,20 +101,18 @@ def scrapeTextAndSave(mode, url):
     year_index = filtered_links.index(next(link for link in filtered_links if "1900" in link))
     year_link = filtered_links[year_index]
     year_url = urljoin(ANNO_URL, year_link)
+    print(year_url)
 
     year_soup = fetchPage(year_url)
-    year_magazine_links = getLinks(mode, year_soup)
-    # print(year_magazine_links)
+    year_magazine_links = getDiffPublicationModeLinks(mode, year_soup, ['anno-plus?', '&datum'], ['anno?', '&datum'])
 
     # Magazine Level
     for magazine in year_magazine_links:
         combined_url = urljoin(year_url, magazine)
         # print(combined_url)
         magazine_soup = fetchPage(combined_url)
-        if mode == ZEITSCHRIFT_BASE_URL:
-            page_links = extractLinks(magazine_soup, ['page'])
-        else:
-            page_links = extractLinks(magazine_soup, ['seite'])
+
+        page_links = getDiffPublicationModeLinks(mode, magazine_soup, ['page'], ['seite'])
         #print(page_links)
 
         # Page Level
@@ -122,16 +122,13 @@ def scrapeTextAndSave(mode, url):
             combined_page_url = urljoin(combined_url, page_url)
             page_soup = fetchPage(combined_page_url)
 
-            if mode == ZEITSCHRIFT_BASE_URL:
-                text_links = extractLinks(page_soup, ['window.open(\'annoshow'])
-            else:
-                text_links = extractLinks(page_soup, ['window.open(\'/cgi-content/annoshow'])
+            text_links = getDiffPublicationModeLinks(mode, page_soup, ['window.open(\'annoshow'],
+                                                     ['window.open(\'/cgi-content/annoshow'])
 
             if text_links:
                 text_url = text_links[0]
                 pattern = r"window.open\('(.*?)',"
                 extracted_text_link = extractText(text_url, pattern)
-
 
                 if extracted_text_link:
                     beginning_url_text = 'https://anno.onb.ac.at/cgi-content/'
@@ -141,6 +138,7 @@ def scrapeTextAndSave(mode, url):
                     text = text_soup.get_text()
                     printable_text = ''.join(char for char in text if char in string.printable)
                     print(printable_text)
+
                     if mode == ZEITSCHRIFT_BASE_URL:
                         publication_mode = "Zeitschrift"
                     else:
@@ -154,8 +152,7 @@ def main():
     with open('validstubs.txt', 'r') as file:
         # Publication Level
         # for line in file:   When everything else is ready, we can loop through each magazine stub like this
-        # line = file.readline().strip()
-        line = "https://anno.onb.ac.at/cgi-content/anno?aid=aog"
+        line = file.readline().strip()
         # sehr hübsche Lösung babe, ich bin entzückt <3
         # At the moment, we only read the first line in validstubs.txt
         if "anno-plus" in line:
